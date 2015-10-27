76f14af897053c836ae8531393bbdfe9eec4430e
clarklight <clarkswork@gmail.com>
2015-10-27 16:15:48 +0800

    Update test.js

--- www/test.js
+++ www/test.js
@@ -1,1 +1,39 @@
-test
+function init (options, successCallback, failureCallback){
+
+cordova.exec(successCallback, failureCallback, 'MifarePlugin', 'init',[options]);
+}
+
+
+
+var nfc = {
+
+    addTagDiscoveredListener: function (callback, win, fail) {
+        document.addEventListener("tag", callback, false);
+        cordova.exec(win, fail, "MifarePlugin", "registerTag", []);
+    },
+
+    addMimeTypeListener: function (mimeType, callback, win, fail) {
+        document.addEventListener("ndef-mime", callback, false);
+        cordova.exec(win, fail, "MifarePlugin", "registerMimeType", [mimeType]);
+    },
+
+    addNdefListener: function (callback, win, fail) {
+        document.addEventListener("ndef", callback, false);
+        cordova.exec(win, fail, "MifarePlugin", "registerNdef", []);
+    },
+
+    addNdefFormatableListener: function (callback, win, fail) {
+        document.addEventListener("ndef-formatable", callback, false);
+        cordova.exec(win, fail, "MifarePlugin", "registerNdefFormatable", []);
+    },
+
+    write: function (ndefMessage, win, fail) {
+        cordova.exec(win, fail, "MifarePlugin", "writeTag", [ndefMessage]);
+    },
+
+    makeReadOnly: function (win, fail) {
+        cordova.exec(win, fail, "MifarePlugin", "makeReadOnly", []);
+    },
+
+
+};
